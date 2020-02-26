3087575856b594890439a8ca94a5b3e72ff7c231
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-02-26 13:32:38 +0000

    :question: Report on the charts uploaded and not uploaded

--- scripts/push-chart.sh
+++ scripts/push-chart.sh
@@ -2,6 +2,9 @@
 
 set +e
 
+existing_charts=()
+uploaded_charts=()
+
 for chart in *.tgz; do
   chartname=$(helm show chart "$chart" | grep ^name: | cut -d ' ' -f 2)
   version=$(helm show chart "$chart" | grep ^version: | cut -d ' ' -f 2)
@@ -10,8 +13,13 @@ for chart in *.tgz; do
   retval=$?
   if [[ $retval -eq 0 ]]; then
     echo "$chart exists, not going to push again."
+    existing_charts+=("$chart")
   else
     echo "Error response - indicates $chart doesn't exist, will attempt to push to repo."
     az acr helm push -n nhsuk "$chart" --verbose
+    uploaded_charts+=("$chart")
   fi
 done
+
+echo "Existing charts:" "${existing_charts[@]-NONE}"
+echo "Uploaded charts:" "${uploaded_charts[@]-NONE}"
